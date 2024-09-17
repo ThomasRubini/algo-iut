6462f406e1e97258b9419c686b46ddf3a5b837e3
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2024-09-17 02:00:58 +0200

    add scanutils.Number()

--- internal/transpiler/scanutils/mod.go
+++ internal/transpiler/scanutils/mod.go
@@ -2,12 +2,22 @@ package scanutils
 
 import (
 	"fmt"
+	"strconv"
 	"text/scanner"
 )
 
 func ScanAndReturn(s *scanner.Scanner) string {
 	s.Scan()
 	return s.TokenText()
+}
+
+func Number(s *scanner.Scanner) int {
+	str := ScanAndReturn(s)
+	num, err := strconv.Atoi(str)
+	if err != nil {
+		panic(fmt.Sprintf("failed to convert %s to int: %v", str, err))
+	}
+	return num
 }
 
 func Must(s *scanner.Scanner, str string) {
