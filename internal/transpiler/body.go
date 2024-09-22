835892a31727b50fd321e4863ca0f3340cef2c7a
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2024-09-22 19:43:45 +0200

    add basic per-line error handling

--- internal/transpiler/body.go
+++ internal/transpiler/body.go
@@ -7,6 +7,8 @@ import (
 	"algo-iut-1/internal/transpiler/translate"
 	"fmt"
 	"io"
+	"os"
+	"strings"
 	"text/scanner"
 )
 
@@ -39,46 +41,69 @@ func doReturn(s *scanner.Scanner, output io.WriteCloser) {
 	output.Write([]byte(fmt.Sprintf("return %s;", value)))
 }
 
+func showError(s *scanner.Scanner, src string, errStr interface{}) {
+	lines := strings.Split(src, "\n")
+	line := lines[s.Pos().Line-1]
+
+	fmt.Printf("Transpiler error: line %v\n", s.Pos().Line)
+	fmt.Println(line)
+	fmt.Println(strings.Repeat(" ", s.Pos().Column-1) + "^")
+	fmt.Println(errStr)
+}
+
 // scan a function/procedure body. Returns when encountering "fin"
 func doBody(s *scanner.Scanner, output io.WriteCloser, src string) {
 	tabsPrefix := tabanalyser.Do(src)
-
 	for {
-		// write tabs/space prefix
-		prefix := tabsPrefix[s.Pos().Line-1]
-		output.Write([]byte(prefix))
-
-		tok := s.TokenText()
-		switch tok {
-		// conditions
-		case "si":
-			s.Scan()
-			doCondition(s, output)
-		// loops
-		case "pour":
-			s.Scan()
-			loops.DoPourLoop(s, output)
-		case "boucle":
-			s.Scan()
-			loops.DoInfiniteLoop(s, output)
-		case "ffaire":
-			s.Scan()
-			output.Write([]byte("}"))
-		// others
-		case "declarer":
-			s.Scan()
-			doDeclare(s, output)
-		case "renvoie":
-			s.Scan()
-			doReturn(s, output)
-		case "fin":
-			s.Scan()
-			output.Write([]byte("}\n"))
-			return
-		default:
-			doLValueStart(s, output)
+		if !doLine(s, output, tabsPrefix, src) {
+			break
 		}
-		output.Write([]byte("\n"))
-
 	}
+}
+
+func doLine(s *scanner.Scanner, output io.WriteCloser, tabsPrefix []string, src string) bool {
+	defer func() {
+		if r := recover(); r != nil {
+			showError(s, src, r)
+			os.Exit(1)
+		}
+	}()
+
+	// write tabs/space prefix
+	prefix := tabsPrefix[s.Pos().Line-1]
+	output.Write([]byte(prefix))
+
+	tok := s.TokenText()
+	switch tok {
+	// conditions
+	case "si":
+		s.Scan()
+		doCondition(s, output)
+	// loops
+	case "pour":
+		s.Scan()
+		loops.DoPourLoop(s, output)
+	case "boucle":
+		s.Scan()
+		loops.DoInfiniteLoop(s, output)
+	case "ffaire":
+		s.Scan()
+		output.Write([]byte("}"))
+	// others
+	case "declarer":
+		s.Scan()
+		doDeclare(s, output)
+	case "renvoie":
+		s.Scan()
+		doReturn(s, output)
+	case "fin":
+		s.Scan()
+		output.Write([]byte("}\n"))
+		return false
+	default:
+		doLValueStart(s, output)
+	}
+	output.Write([]byte("\n"))
+
+	return true
 }
