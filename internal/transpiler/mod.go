87e332926096748288b76bc7dd02c9b71583299d
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2024-09-23 02:34:09 +0200

    improve DoRoot() invalid token handling

--- internal/transpiler/mod.go
+++ internal/transpiler/mod.go
@@ -3,20 +3,20 @@ package transpiler
 import (
 	"algo-iut-1/internal/langoutput"
 	"algo-iut-1/internal/scan"
-	"fmt"
 )
 
 func DoRoot(s scan.Scanner, output langoutput.T, src string) {
 	for s.HasMore() {
 		tok := s.Peek()
-		s.Advance()
 		switch tok {
 		case "fonction":
+			s.Advance()
 			doFunction(s, output, src)
 		case "procedure":
+			s.Advance()
 			doProcedure(s, output, src)
 		default:
-			panic(fmt.Sprintf("unexpected token '%s'", tok))
+			s.InvalidToken("")
 		}
 	}
 }
