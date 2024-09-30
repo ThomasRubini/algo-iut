9debe16f3c973911f4db2ee94f404ea7ac0c338b
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2024-09-30 18:09:48 +0200

    use monaco editor

--- web/content/script.js
+++ web/content/script.js
@@ -19,3 +19,33 @@ function initWasm() {
     }
     );
 }
+
+var leftEditorInst = null;
+var rightEditorInst = null;
+
+async function initMonaco() {
+    require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@latest/min/vs' }});
+    window.MonacoEnvironment = { getWorkerUrl: () => proxy };
+    
+    let proxy = URL.createObjectURL(new Blob([`
+        self.MonacoEnvironment = {
+            baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
+        };
+        importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');
+    `], { type: 'text/javascript' }));
+    
+    let promise = new Promise((resolve, reject) => {
+        require(["vs/editor/editor.main"], function () {
+            leftEditorInst = monaco.editor.create(document.getElementById('leftEditorTag'), {
+                language: 'javascript',
+                theme: 'vs-dark'
+            });
+            rightEditorInst = monaco.editor.create(document.getElementById('rightEditorTag'), {
+                language: 'javascript',
+                theme: 'vs-dark'
+            });    
+            resolve();
+        });
+    });
+    await promise;
+}
