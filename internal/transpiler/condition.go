650e6c74e4e081d214559a499d36fdffef3948c7
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2024-10-05 20:49:11 +0200

    simplify

--- internal/transpiler/condition.go
+++ internal/transpiler/condition.go
@@ -4,7 +4,6 @@ import (
 	"algo-iut/internal/langoutput"
 	"algo-iut/internal/scan"
 	"algo-iut/internal/transpiler/translate"
-	"fmt"
 )
 
 func doElseIf(s scan.Scanner, output langoutput.T) {
@@ -16,5 +15,5 @@ func doCondition(s scan.Scanner, output langoutput.T) {
 	condition := translate.Expr(s.Expr())
 	s.Must(")")
 
-	output.Write(fmt.Sprintf("if (%v) {", condition))
+	output.Writef("if (%v) {", condition)
 }
