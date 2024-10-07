60cf3f1d2cf84334711a1a738bad15f8cbe44f24
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2024-10-07 20:07:40 +0200

    add tests to check output C++ validity

--- tests/run_test.go
+++ tests/run_test.go
@@ -4,10 +4,10 @@ import (
 	"algo-iut/internal/langoutput"
 	"algo-iut/internal/scan"
 	"algo-iut/internal/transpiler"
-	"algo-iut/internal/utils/nopwritecloser"
-	"bytes"
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 	"testing"
 
 	"path/filepath"
@@ -36,7 +36,7 @@ func readDir(dirPath string) []string {
 	return filepaths
 }
 
-func testOneSyntax(filepath string) (err error) {
+func testOneSyntax(filepath string) (cpp_data string, err error) {
 	codeBytes, err := os.ReadFile(filepath)
 	if err != nil {
 		// this is an error in the test itself, not in the transpiler
@@ -44,11 +44,24 @@ func testOneSyntax(filepath string) (err error) {
 	}
 
 	src := string(codeBytes)
-	lang_output := bytes.Buffer{}
+	lang_output := langoutput.NewString()
 
-	transpile_err := transpiler.Do(scan.New(src), langoutput.NewWriteCloser(nopwritecloser.New(&lang_output)), src)
+	transpile_err := transpiler.Do(scan.New(src), lang_output, src)
 	if transpile_err != nil {
-		return fmt.Errorf("transpile error: %v", transpile_err)
+		return "", fmt.Errorf("transpile error: %v", transpile_err)
+	}
+
+	return lang_output.String(), nil
+}
+
+func checkOneCpp(cpp_data string) (err error) {
+	cmd := exec.Command("g++", "-x", "c++", "-o", "/dev/null", "-")
+	cmd.Stdin = strings.NewReader(cpp_data)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("gcc error: %v", err)
 	}
 
 	return nil
@@ -59,7 +72,7 @@ func TestSyntax(t *testing.T) {
 
 	for _, entry := range readDir("syntax/") {
 		t.Run(entry, func(t *testing.T) {
-			err := testOneSyntax(entry)
+			_, err := testOneSyntax(entry)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -72,7 +85,7 @@ func TestSyntaxFail(t *testing.T) {
 
 	for _, entry := range readDir("syntax_fail/") {
 		t.Run(entry, func(t *testing.T) {
-			err := testOneSyntax(entry)
+			_, err := testOneSyntax(entry)
 			if err == nil {
 				t.Fatal("expected error")
 			}
@@ -87,9 +100,21 @@ func TestExamples(t *testing.T) {
 
 	for _, entry := range readDir("examples/") {
 		t.Run(entry, func(t *testing.T) {
-			err := testOneSyntax(entry)
-			if err != nil {
-				t.Fatal(err)
+			var cpp_data string
+			t.Run("Transpile", func(t *testing.T) {
+				var err error
+				cpp_data, err = testOneSyntax(entry)
+				if err != nil {
+					t.Fatal(err)
+				}
+			})
+			if cpp_data != "" {
+				t.Run("CheckCpp", func(t *testing.T) {
+					err := checkOneCpp(cpp_data)
+					if err != nil {
+						t.Fatal(err)
+					}
+				})
 			}
 		})
 	}
